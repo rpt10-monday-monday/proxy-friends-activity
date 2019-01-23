f1f0454532de4fa52bbe817523d59b3fff400f75
Malcolm Jackson <mj@deepplanet.com>
2019-01-22 18:42:38 -0800

    updating proxy middlewear settings

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ var proxy = require('http-proxy-middleware');
 
 const app = express();
 
+app.use(proxy("/songs", {target: 'http://ec2-3-87-34-19.compute-1.amazonaws.com/'}));
+app.use(proxy("/filterSongsRock", {target: 'http://ec2-3-87-34-19.compute-1.amazonaws.com:5000/'}));
+app.use(proxy("/filterSongsJazz", {target: 'http://ec2-3-87-34-19.compute-1.amazonaws.com:5000/'}));
+app.use(proxy("/filterSongsPop", {target: 'http://ec2-3-87-34-19.compute-1.amazonaws.com:5000/'}));
+
+app.use(proxy("/data", {target: 'http://friends-component-fec.us-west-1.elasticbeanstalk.com/'}));
+app.use(proxy("/song", {target: 'http://friends-component-fec.us-west-1.elasticbeanstalk.com'}));
+
 // change to env 
 const port = process.env.PORT || 3000;
 // var ip = '127.0.0.1';
@@ -23,12 +31,7 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(proxy("/songs", {target: 'http://ec2-3-87-34-19.compute-1.amazonaws.com/'}));
-app.use(proxy("/filterSongsRock", {target: 'http://ec2-3-87-34-19.compute-1.amazonaws.com:5000/'}));
-app.use(proxy("/filterSongsJazz", {target: 'http://ec2-3-87-34-19.compute-1.amazonaws.com:5000/'}));
-app.use(proxy("/filterSongsPop", {target: 'http://ec2-3-87-34-19.compute-1.amazonaws.com:5000/'}));
-app.use(proxy("/data", {target: 'http://friends-component-fec.us-west-1.elasticbeanstalk.com/'}));
-app.use(proxy("/song", {target: 'http://friends-component-fec.us-west-1.elasticbeanstalk.com'}));
+
 
 app.get('/', function (req, res) {
   res.status(200).send('GET request from the homepage');
