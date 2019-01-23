2abbdb58ad6d2d630413b81a26fae2f12f21b476
Malcolm Jackson <mj@deepplanet.com>
2019-01-22 18:31:10 -0800

    updating proxy middlewear settings

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(proxy("/filterSongsRock", {target: 'http://ec2-3-87-34-19.compute-1.amaz
 app.use(proxy("/filterSongsJazz", {target: 'http://ec2-3-87-34-19.compute-1.amazonaws.com:5000/'}));
 app.use(proxy("/filterSongsPop", {target: 'http://ec2-3-87-34-19.compute-1.amazonaws.com:5000/'}));
 app.use(proxy("/data", {target: 'http://friends-component-fec.us-west-1.elasticbeanstalk.com/'}));
-app.use(proxy("/song", {target: 'http://friends-component-fec.us-west-1.elasticbeanstalk.com/'}));
+app.use(proxy("/song", {target: 'http://friends-component-fec.us-west-1.elasticbeanstalk.com/:3000'}));
 
 app.get('/', function (req, res) {
   res.status(200).send('GET request from the homepage');
